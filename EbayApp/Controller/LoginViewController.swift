b07a2e942f16be5ea59813c1b83b5c4076acdd20
PACE <pace@pc242743.local>
2017-12-04 14:50:11 +0530

    From Sharmila

---
+++ EbayApp/Controller/LoginViewController.swift
@@ -0,0 +1,147 @@
+import UIKit
+import Foundation
+
+class LoginViewController: UIViewController
+{
+    @IBOutlet weak var emaillbl: UITextField!
+    
+    @IBOutlet weak var pwdlbl: UITextField!
+    
+    @IBOutlet weak var signinLbl: UIButton!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+      /*  let preferences = UserDefaults.standard
+        if(preferences.object(forKey: "session") != nil)
+        {
+            LoginDone()
+        }
+        else
+        {
+            LoginToDo()
+        }
+        
+    }
+*/
+    }
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+    @IBAction func signinbutton(_ sender: Any)
+    {
+        
+       /* if(signinLbl.titleLabel?.text == "Logout")
+        {
+            let preferences = UserDefaults.standard
+            
+            preferences.removeObject(forKey: "session")
+            LoginToDo()
+           return
+        }
+        
+           */
+        
+        let emailid = emaillbl.text
+        let password = pwdlbl.text
+        
+       // DoLogin(emaillbl: emailid!, pwdlbl: password!)
+        
+        if(emailid == ""  || password == "")
+        {
+           // return
+               // self.ViewController(SellProductViewController, animated:true, completion:nil)
+        }
+    }
+        
+       /*  func DoLogin(emaillbl:String, pwdlbl:String)
+        {
+            
+        
+        
+            let url = URL(string: "https://example.com/api/v1/records.json")
+            //let url = URL(string: "http://www.kaleidosblog.com/tutorials/login/api/login√")
+            
+            let session = URLSession.shared
+            
+            let request = NSMutableURLRequest(url: url!)
+            request.httpMethod = "POST"
+            let paramToSend = "emailid" + emaillbl + "&password=" + pwdlbl
+
+            request.httpBody = paramToSend.data(using: String.Encoding.utf8)
+            
+            let task = session.dataTask(with: request as URLRequest, completionHandler : {
+                (data, response, error) in
+                guard let _:Data = data else
+                {
+                    print(" test1")
+                    return
+                }
+                let json:Any?
+                
+                do
+                {
+                    json = try JSONSerialization.jsonObject(with: data!, options: [])
+                }
+                catch
+                {   print(" test2")
+                    return
+                }
+                
+                guard let server_response = json as? NSDictionary else
+                {
+                     print(" test3")
+                    return
+                }
+                
+                
+                if let data_block = server_response["data"] as? NSDictionary
+                {
+                    if let session_data = data_block["session"] as? String
+                    {
+                        let preferences = UserDefaults.standard
+                        preferences.set(session_data, forKey: "session")
+                        DispatchQueue.main.async(
+                            execute:self.LoginDone
+                        )
+                    
+                    }
+                }
+                
+                
+            })
+            
+            
+            
+            
+        
+      task.resume()
+            
+
+}
+    
+    
+func LoginToDo()
+{
+    emaillbl.isEnabled = true
+    pwdlbl.isEnabled = true
+    signinLbl.setTitle("Login", for: .normal)
+    
+}
+func LoginDone()
+{
+    emaillbl.isEnabled = false
+    pwdlbl.isEnabled = false
+    signinLbl.setTitle("Logout", for: .normal)
+    
+}
+
+}
+*/
+}
+
--- EbayApp/Controller/ViewController.swift
+++ EbayApp/Controller/ViewController.swift
@@ -1,147 +1,118 @@
-import UIKit
-import Foundation
+//
+//  ViewController.swift
+//  Auction
+//
+//  Created by Roshan on 21/11/17.
+//  Copyright © 2017 Roshan. All rights reserved.
+//
 
-class ViewController: UIViewController
-{
-    @IBOutlet weak var emaillbl: UITextField!
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    var name:NSArray = []
+    var imageArr:NSArray = []
+    var BidPrice: NSArray = []
+    var ExpiryTime: NSArray = []
+    var StartTime: NSArray = []
+    var Description: NSArray = []
+    var UserId: NSArray = []
     
-    @IBOutlet weak var pwdlbl: UITextField!
-    
-    @IBOutlet weak var signinLbl: UIButton!
-    
-    
-    override func viewDidLoad()
-    {
+    var selectedSegment = 1
+    let array2 = ["1","2","3"]
+    override func viewDidLoad() {
         super.viewDidLoad()
-      /*  let preferences = UserDefaults.standard
-        if(preferences.object(forKey: "session") != nil)
+        // Do any additional setup after loading the view, typically from a nib.
+        name = [ "WagonR","Karizma","Rolex Watch","HP Pavilion","Red Shoes","TV"]
+        imageArr = [UIImage(named: "1")!,UIImage(named: "2")!, UIImage(named: "3")!, UIImage(named: "4")!,UIImage(named: "5")!, UIImage(named: "6")!]
+        BidPrice = [ "Current Bid Price = 10,000 $ ","Current Bid Price = 8,000 $ ","Current Bid Price = 1200 $ ","Current Bid Price = 6500 $ ","Current Bid Price = 200 $ ","Current Bid Price = 400 $ "]
+        ExpiryTime = [ "Expiry Time = 12:30 PM","Expiry Time = 4:30 PM","Expiry Time = 2:30 AM","Expiry Time = 8:05 AM","Expiry Time = 9:30 PM","Expiry Time = 5:55 AM"]
+        
+        StartTime = [ "Start Time = 12:30 AM","Start Time = 2:30 AM","Start Time = 9:30 PM","Start Time = 8:25 PM","Start Time = 8:30 AM","Start Time = 5:25 PM"]
+        Description = [ "Don't cry because it's over, smile because it happened","Be yourself everyone else is already taken AM","Without music life would be a mistake","So many books, so little time","Be the change that you wish to see in the world.","Don't cry because it's over, smile because it happened."]
+        UserId = [ "User Id = US1701","User Id = US1702","User Id = US1703","User Id = US1704","User Id = US1705","User Id = US1706"]
+        
+        
+        self.TableviewLbl.delegate = self
+        self.TableviewLbl.dataSource = self
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var TableviewLbl: UITableView!
+    
+    @IBAction func SegmentLbl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            
+            selectedSegment = 1
+        }
+        else {
+            selectedSegment = 2
+        }
+        self.TableviewLbl.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if selectedSegment == 1
         {
-            LoginDone()
+            return name.count
+            
         }
         else
         {
-            LoginToDo()
+            return array2.count
         }
         
     }
-*/
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // let  cell = tableview.dequ
+        
+        let cell1 = tableView.dequeueReusableCell(withIdentifier: "Cell1") as! TableViewCell
+        
+        cell1.ProdImage.image = imageArr[indexPath.row] as? UIImage
+        cell1.lblName.text! = name[indexPath.row] as! String
+        
+        cell1.BidPriceLbl.text! = BidPrice[indexPath.row] as! String
+        cell1.ExpiryTimeLbl.text! = ExpiryTime[indexPath.row] as! String
+        cell1.UserIdLbl.text! = UserId[indexPath.row] as! String
+        
+        let cell2 = tableView.dequeueReusableCell(withIdentifier: "Cell2") as! TableViewCell
+        
+        cell2.ProdImage.image = imageArr[indexPath.row] as? UIImage
+        // cell2.lblName.text! = name[indexPath.row] as! String
+        
+        //cell2.BidPriceLbl.text! = BidPrice[indexPath.row] as! String
+        //cell2.ExpiryTimeLbl.text! = ExpiryTime[indexPath.row] as! String
+        //cell2.UserIdLbl.text! = UserId[indexPath.row] as! String
+        if selectedSegment == 1
+        {
+            return cell1
+            
+        }
+        else
+        {
+            return cell2
+        }
     }
-    override func didReceiveMemoryWarning()
+    func tableView(_ tableview: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        super.didReceiveMemoryWarning()
+        let Storyboard = UIStoryboard(name: "Main",bundle: nil)
+        let DvC = Storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        DvC.getImage = imageArr[indexPath.row] as! UIImage
+        DvC.getname = name[indexPath.row] as! String
+        DvC.getDCurrentBidPrice = BidPrice[indexPath.row] as! String
+        DvC.getDescrition = Description[indexPath.row] as! String
+        DvC.getDExpiryTime = ExpiryTime[indexPath.row] as! String
+        DvC.getStartTime = StartTime[indexPath.row] as! String
         
+        DvC.getDUserId = UserId[indexPath.row] as! String
+        
+        
+        self.navigationController?.pushViewController(DvC, animated: true)
     }
-
-
-    @IBAction func signinbutton(_ sender: Any)
-    {
-        
-       /* if(signinLbl.titleLabel?.text == "Logout")
-        {
-            let preferences = UserDefaults.standard
-            
-            preferences.removeObject(forKey: "session")
-            LoginToDo()
-           return
-        }
-        
-           */
-        
-        let emailid = emaillbl.text
-        let password = pwdlbl.text
-        
-       // DoLogin(emaillbl: emailid!, pwdlbl: password!)
-        
-        if(emailid == ""  || password == "")
-        {
-           // return
-               // self.ViewController(SellProductViewController, animated:true, completion:nil)
-        }
-    }
-        
-       /*  func DoLogin(emaillbl:String, pwdlbl:String)
-        {
-            
-        
-        
-            let url = URL(string: "https://example.com/api/v1/records.json")
-            //let url = URL(string: "http://www.kaleidosblog.com/tutorials/login/api/login√")
-            
-            let session = URLSession.shared
-            
-            let request = NSMutableURLRequest(url: url!)
-            request.httpMethod = "POST"
-            let paramToSend = "emailid" + emaillbl + "&password=" + pwdlbl
-
-            request.httpBody = paramToSend.data(using: String.Encoding.utf8)
-            
-            let task = session.dataTask(with: request as URLRequest, completionHandler : {
-                (data, response, error) in
-                guard let _:Data = data else
-                {
-                    print(" test1")
-                    return
-                }
-                let json:Any?
-                
-                do
-                {
-                    json = try JSONSerialization.jsonObject(with: data!, options: [])
-                }
-                catch
-                {   print(" test2")
-                    return
-                }
-                
-                guard let server_response = json as? NSDictionary else
-                {
-                     print(" test3")
-                    return
-                }
-                
-                
-                if let data_block = server_response["data"] as? NSDictionary
-                {
-                    if let session_data = data_block["session"] as? String
-                    {
-                        let preferences = UserDefaults.standard
-                        preferences.set(session_data, forKey: "session")
-                        DispatchQueue.main.async(
-                            execute:self.LoginDone
-                        )
-                    
-                    }
-                }
-                
-                
-            })
-            
-            
-            
-            
-        
-      task.resume()
-            
-
-}
-    
-    
-func LoginToDo()
-{
-    emaillbl.isEnabled = true
-    pwdlbl.isEnabled = true
-    signinLbl.setTitle("Login", for: .normal)
-    
-}
-func LoginDone()
-{
-    emaillbl.isEnabled = false
-    pwdlbl.isEnabled = false
-    signinLbl.setTitle("Logout", for: .normal)
-    
-}
-
-}
-*/
 }
 
